62d61c0e1a520b6ecc4a68db26a3aad4df25038a
puni2426 <53472959+puni2426@users.noreply.github.com>
2019-10-01 16:33:29 +0530

    Update hamiltonian and lagrangion.c

--- hamiltonian and lagrangion.c
+++ hamiltonian and lagrangion.c
@@ -1,25 +1,41 @@
 /*hamiltonian and lagrangian in c*/
 #include<stdio.h>
-
-void main()
+int main()
 {
-int n;
-printf("Enter the number of values\n");
+int n,j;
+printf("enter the number of values \n");
 scanf("%d",&n);
-int a[n],count=0;
+int a[n];
+printf("enter the array elements\n");
 for(int i=0;i<n;i++)
-printf("Enter the array elements\n");
 scanf("%d",&a[i]);
-for(int i=0;i<n-1;i++)
+int great=a[0];
+printf("the output of the array elements are\n");
+for(int i=1;i<n;i++)
 {
-count = n-1-i;
-for(int j=i+1;j<n;j++)
+if(a[i]>great)
 {
-if(a[i]>=a[j]) count=count - 1;
-else break;
+great=a[i];
+j=i;
 }
-if(count == 0)
+}
+printf("%d ",a[j]);
+for(int i=j+1;i<n;i++)
+{ if(a[i]==great)
+{
 printf("%d ",a[i]);
+continue;
+}
+for(int k=i+1;k<n;k++)
+{
+if(a[k]>=a[i])
+break;
+else
+{
+if(k>=n-1)
+printf("%d ",a[i]);
+}
+}
 }
 printf("%d",a[n-1]);
 }
